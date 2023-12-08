6d06b0eba2aa7d39d9bd17d79849bc1573f5420b
Younes Moukhlij <youmoukh@e1r2p8.1337.ma>
2023-12-08 12:46:02 +0100

    Exam 02 Level 01

---
+++ Level 01/rev_print/rev_print.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+int main(int ac, char **av)
+{
+	int	l;
+
+	l = 0;
+	if (ac == 2)
+	{
+		while (av[1][l])
+			l++;
+		l -= 1;
+		while (l >= 0)
+			write(1, &av[1][l--], 1);
+	}
+	write(1, "\n", 1);
+	return (0);
+}
